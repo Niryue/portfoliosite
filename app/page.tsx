bbe33a781eef07c0b711e5a581bb13856b3223e0
Niryue <kehanyuhankyu@gmail.com>
2026-07-10 23:07:57 -0800

    added main page artistic design, functional aspects untouched mostly.

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,276 @@
-import Image from "next/image";
+"use client";
+
+// import { useEffect, useRef } from "react";
+
+
+// export default function Home() {
+//   const videoRef = useRef<HTMLVideoElement>(null);
+
+//   useEffect(() => {
+//     if (videoRef.current) {
+//       videoRef.current.playbackRate = 0.5; // Half speed
+//     }
+//   }, []);
+//   return (
+//     <main style={{ width: "100vw", height: "100vh" }}>
+//       <video
+//         ref={videoRef}
+//         autoPlay
+//         loop
+//         muted
+//         playsInline
+//         className="fixed inset-0 w-full h-full object-cover -z-10"
+//       >
+//         <source src="/videos/mainpg.mp4" type="video/mp4" />
+//       </video>
+      
+
+//     </main>
+//   );
+// }
+
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { useVideoTexture, useTexture, useGLTF, Sparkles, FlyControls } from "@react-three/drei";
+import * as THREE from "three";
+import { useEffect, useRef, useState, useMemo } from "react";
+import { EffectComposer, SelectiveBloom, Selection, Select } from "@react-three/postprocessing";
+
+
+function VideoPlane() {
+  const texture = useVideoTexture("/videos/mainpg.mp4");
+  
+  useEffect(() => {
+    const video = texture.image;
+
+    if (video instanceof HTMLVideoElement) {
+      video.playbackRate = 0.5; // Half speed
+    }
+  }, [texture]);
+  return (
+    <mesh
+      position={[0, 0, 0]}>
+      <planeGeometry args={[4, 2.25]} />
+      <meshBasicMaterial map={texture} toneMapped={false} fog={false}/>
+    </mesh>
+  );
+}
+
+function TexturePlane({ texturePath, position, scale, opacity, phase, hovered }) {
+  const texture = useTexture(texturePath);
+  const ref = useRef();
+  useFrame(({ clock }) => {
+    const t = clock.getElapsedTime() % (Math.PI * 2);
+
+  ref.current.position.y =
+      position[1] + Math.sin(t * 2 + phase) * 0.03;
+  });
+
+  return (//make later a glow effect only appear when mouse hovers
+    <mesh
+      ref={ref}
+      position={position}
+      scale={scale}
+    >
+      <planeGeometry args={[1, 1]} />
+
+      <meshBasicMaterial
+        map={texture}
+        transparent
+        fog={false}
+        color="white"
+        opacity={hovered ? opacity : 0}
+      />
+
+    </mesh>
+  );
+}
+
+function HoverArea({ hovered, setHovered }) {
+  const { scene: portfolioHover } = useGLTF("/models/portfolioHover.glb");
+  const { scene: nameHover } = useGLTF("/models/nameHover.glb");
+
+  useEffect(() => {
+      [portfolioHover, nameHover].forEach((scene) => {
+        scene.traverse((child) => {
+          if (child.isMesh) {
+            child.material.transparent = true;
+            child.material.opacity = 0;
+          }
+        });
+      });
+    }, [portfolioHover, nameHover]);
+
+  return (
+    <group>
+      <primitive
+        object={portfolioHover}
+        onPointerOver={() => setHovered("Portfolio")}
+        onPointerOut={() => setHovered(null)}
+        position={[0.22, -0.11, 0.05]}
+        scale={0.56}
+        rotation={[0, Math.PI/2, 0.15]}
+        rotation-order="ZYX"
+      >
+      </primitive>
+
+      <primitive
+        object={nameHover}
+        onPointerOver={() => setHovered("Name")}
+        onPointerOut={() => setHovered(null)}
+        position={[-0.59, -0.52, 0.05]}
+        scale={1.2}
+        rotation={[0, Math.PI/2, 0.05]}
+        rotation-order="ZYX"
+      >
+      </primitive>
+    </group>
+  );
+}
+
+function Floor() {
+  return (
+    <mesh
+      position={[0, -5, 0]}
+      rotation={[-Math.PI / 2, 0, 0]}
+      receiveShadow
+    >
+      <planeGeometry args={[50, 50]} />
+      <meshStandardMaterial 
+      color="#ffffff"
+      roughness={0.5}/>
+    </mesh>
+  );
+}
+
+function Pillar({ position }) {
+  const { scene } = useGLTF("/models/pillar.glb");
+  const clonedScene = useMemo(() => scene.clone(), [scene]);
+  useEffect(() => {
+    clonedScene.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        child.castShadow = true;
+        }
+      });
+    }, [clonedScene]);
+  
+  const ref = useRef();
+
+  useFrame(() => {
+    ref.current.position.z += 0.006;
+
+    // Reset pillar when it goes past the camera
+      if (ref.current.position.z > 5) {
+        ref.current.position.z = -25;
+      }
+    });
+   return (
+    <primitive
+      ref={ref}
+      object={clonedScene}
+      scale={[0.8, 1, 0.8]}
+      position={position}
+      rotation={[0, 0, 0]}
+    />
+  );
+}
+
+function SceneFog() {
+  const { scene } = useThree();
+
+  useEffect(() => {
+    scene.fog = new THREE.FogExp2("#e0ecff", 0.06);
+  }, [scene]);
+
+  return null;
+}
+// function Dust() { Sparkles particle
+//   return (
+//     <Sparkles
+//       count={500}
+//       size={10}
+//       speed={0.2}
+//       opacity={0.15}
+//       color="yellow"
+//       scale={[1, 0.5, 0.02]}
+//       position={[0, 0, -0.1]}
+//     />
+//   );
+// }
+
+// restricted camera movement based on mouse position
+function CameraRig() {
+  const { camera, pointer } = useThree();
+
+  useFrame(() => {
+    const intensity = 0.15;
+
+    const targetX = pointer.x * intensity;
+    const targetY = pointer.y * intensity;
+
+    camera.position.x = THREE.MathUtils.lerp(camera.position.x, targetX, 0.03);
+    camera.position.y = THREE.MathUtils.lerp(camera.position.y, targetY, 0.03);
+    camera.position.z = 3;
+
+    camera.lookAt(0, 0, 0);
+  });
+
+  return null;
+}
+//free cam
+// function Freecam() {
+//   const { camera, gl } = useThree();
+
+//   return (
+//     <FlyControls
+//       args={[camera, gl.domElement]}
+//       movementSpeed={5}
+//       rollSpeed={0.5}
+//     />
+//   );
+// }
 
 export default function Home() {
+  const pillars = [
+    [-8, -5, -5],
+    [8, -5, -5],
+    [-8, -5, -15],
+    [8, -5, -15],
+    [-8, -5, -25],
+    [8, -5, -25],
+  ];
+  const [hovered, setHovered] = useState(null);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main style={{ width: "100vw", height: "100vh" }}>
+      <Canvas 
+      shadows 
+      camera={{ position: [0, 0, 3], fov: 50 }}
+      style={{ background: "#e0ecff" }}>
+          <CameraRig />
+          {/* <Freecam /> */}
+          <VideoPlane />
+          <directionalLight 
+          position={[100, 20, 30]} 
+          intensity={20} 
+          castShadow
+          shadow-mapSize-width={2048}
+          shadow-mapSize-height={2048}
+          shadow-camera-left={-20}
+          shadow-camera-right={20}
+          shadow-camera-top={20}
+          shadow-camera-bottom={-20} />
+          <ambientLight intensity={2} />
+          <Floor />
+          {pillars.map((pos, i) => (
+            <Pillar key={i} position={pos} />
+          ))}
+          <SceneFog />
+          <TexturePlane texturePath="/images/Portfolio.png" position={[0.47, 0.5, 0.1]} scale={0.7} opacity={1} phase={0} hovered={true}/>
+          <TexturePlane texturePath="/images/whiteblur.png" position={[0.47, 0.5, 0.1]} scale={0.7} opacity={0.55} phase={0} hovered={hovered === "Portfolio"}/>
+          <TexturePlane texturePath="/images/Name.png" position={[-0.7, 0.32, 0.1]} scale={0.7} opacity={1} phase={Math.PI} hovered={true}/>
+          <TexturePlane texturePath="/images/whiteblur.png" position={[-0.7, 0.32, 0.1]} scale={0.7} opacity={0.55} phase={Math.PI} hovered={hovered === "Name"}/>
+          <HoverArea hovered={hovered} setHovered={setHovered} />
+      </Canvas>
+    </main>
   );
 }
